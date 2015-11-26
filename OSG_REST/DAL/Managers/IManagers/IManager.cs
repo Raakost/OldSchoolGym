45c115679da499ce8a21aa6f831064191776fda6
unknown <manifibell@gmail.com>
2015-11-26 10:39:00 +0100

    Created IManager

---
+++ OSG_REST/DAL/Managers/IManagers/IManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Managers.IManagers
+{
+    public interface IManager<T>
+    {
+        IEnumerable<T> ReadAll();
+
+        T ReadByID(int Id);
+
+        T Create(T model);
+
+        Boolean Delete(T model);
+
+        T Update(T model);
+    }
+}
